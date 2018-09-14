6c4c388caa94739763831f504cf05bb95dcec465
zattew <matteomilani50@gmail.com>
2018-09-14 14:55:08 +0200

    Update Batch_Effect.R

--- Batch_Effect.R
+++ Batch_Effect.R
@@ -12,7 +12,8 @@ hc1 <- hclust(d, method = "ward.D")
 plot(hc1)
 
 
-pdf() #Clustering in base al batch
+pdf( ... ,height=5,width=8) #Clustering in base al batch
+par(xpd=F)
 
 for( i in ... ) #colonne in cui ci sono le caratteristiche di interesse (es 7:10)
 {
@@ -22,15 +23,24 @@ for( i in ... ) #colonne in cui ci sono le caratteristiche di interesse (es 7:10
   d <- dist(df, method = "euclidean")
   hc1 <- hclust(d, method = "ward.D" )
   
-  par(cex=0.6)
-  plot(hc1,main=names(pData(eset))[i]
+  plot(hc,
+     cex=0.6,
+     xlab="",
+     ylab="Euclidean distance",
+     hang=-1,
+     main="Distance between samples")
+  
   abline(h= ... ,col="red",lty=2,lwd=2)
-  
-  text(15, 3, labels = "Cluster1", pos=3,cex=2)
-  text(62, 3,"Cluster2",pos=3,cex=2)
-  
+  text(24,8,"Cluster 1",cex=1.1,font=2)
+  text(53.5,8,"Cluster 2",cex=1.1,font=2)
+
   eset$batch <- cutree(hc1,h= ...)
 
+  table(eset$clustertree,as.character(unlist(pData(eset)[i])))
+
+  ##Test del X^2
+  chisq.test(eset$batch,as.character(unlist(eset$description)), correct=FALSE) 
+  
   boxplot( as.numeric(unlist(pData(eset)[i])) ~ eset$batch,
           main=names(pData(eset))[i], names=c("cluster1","cluster2"))
           
